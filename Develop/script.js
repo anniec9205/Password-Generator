2effbfedeacfa81c09b1cf9c311a369e0732f506
anniec9205 <danbi9205@gmail.com>
2021-03-15 21:03:11 +1100

    created the prompts

--- Develop/script.js
+++ Develop/script.js
@@ -17,4 +17,15 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Prompts that come up after you click generate password
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 8 but less than 128.");
+
+  var numbers = confirm("Do you want numbers in your password?");
+
+  var lowerCases = confirm("Do you want lowercases in your password?");
+
+  var upperCases = confirm("Do you want uppercases in your password?");
+
+  var special = confirm("Do you want special characters in your password?");
 
